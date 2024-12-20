1493220a091b01d7d6de0cf5b23762ea4abd9d80
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2024-12-20 08:28:33 -0800

    refactor: Rename tailwind.config.js to .ts and update imports for consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
+import tailwindTypography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./lib/**/*.tsx', './src/**/*.tsx'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [tailwindTypography],
 };
