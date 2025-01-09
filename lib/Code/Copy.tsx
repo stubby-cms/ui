f696ddfb162153b3f9c9f667722660eb56f620ac
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2025-01-09 08:11:07 -0800

    fix: fixing the copy button tooltip to have a background

--- lib/Code/Copy.tsx
+++ lib/Code/Copy.tsx
@@ -43,7 +43,7 @@ export function CopyButton({ value, className, src, ...props }: CopyButtonProps)
         <span className="sr-only">Copy</span>
         {hasCopied ? <CheckIcon size={14} /> : <CopyIcon size={14} />}
       </button>
-      <div className="copy-button-tooltip group-hover:block hidden absolute mt-2 left-[-28px] text-center w-20">
+      <div className="group-hover:block hidden absolute mt-2 text-center px-2 py-0.5 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm border dark:border-gray-800">
         {hasCopied ? 'Copied!' : 'Copy'}
       </div>
     </div>
